3fe8e65d245393e132017c892685a23f08b5fc94
Victor Rivas <vctr90w@gmail.com>
2014-08-09 18:43:13 -0430

    Responsive header menu done

---
+++ public/javascripts/app.js
@@ -0,0 +1,1 @@
+angular.module('vctr',['vctr.controllers','vctr.services','vctr.directives','ngRoute']);
---
+++ public/stylesheets/js/headers.js
@@ -0,0 +1,7 @@
+//------------------------HEADER STYLE DRIVER
+
+//Show and hide header menu
+
+$('#responsive-menu').on('click',function(){
+	$('header ul').show();
+});
