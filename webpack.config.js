40fa32cff881b074ae5fe9cd2ca90072b71e4c33
Victor <vctr90w@gmail.com>
2019-06-08 22:01:15 -0400

    Added reloader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const config = require('./config');
 
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
+  watch: true,
+  devServer: {
+    port: config.PORT,
+    host: config.HOST,
+    overlay: true
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
