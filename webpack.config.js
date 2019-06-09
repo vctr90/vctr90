5ae12142eb5c0bfce41c8c7c960a5c4b06b6d683
Victor <vctr90w@gmail.com>
2019-06-09 12:04:13 -0400

    Added first apporoach to menu

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
       {
         test:  /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test:  /\.(png|jpe?g|gif)$/,
+        use: ['file-loader']
       }
     ]
   }
