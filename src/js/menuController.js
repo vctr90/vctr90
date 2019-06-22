bd1021505fae3f267a0a196dcd871ad5ba7edd8a
Victor <vctr90w@gmail.com>
2019-06-21 21:49:25 -0400

    Fixed menu click event

--- src/js/menuController.js
+++ src/js/menuController.js
@@ -4,9 +4,16 @@ const $menuElements = $menu.querySelectorAll('ul li');
 
 const triggerMenuToggle = () => $menu.classList.toggle('show-menu');
 
+let clickedMenu = false;
+
 const navigateToSection = (event) => {
    cleanMenuSelection();
+   event.currentTarget.classList.toggle('selected');
+   clickedMenu = true;
    triggerMenuToggle(); 
+   setTimeout(() => {
+      clickedMenu = false;
+   }, 500)
 };
 
 const cleanMenuSelection = () => $menuElements.forEach(($menuElement) => $menuElement.classList.remove('selected'));
@@ -15,13 +22,16 @@ $menuToggle.onclick = triggerMenuToggle;
 $menuElements.forEach(($menuElement) => $menuElement.onclick = navigateToSection);
 
 $(window).scroll(() => {
-   const scrollDistance = $(window).scrollTop();
+   if (!clickedMenu) {
+      const scrollDistance = $(window).scrollTop();
 
-   const $sections = $('.section');
-   $sections.each(function (currentSectionIndex) {
-      if ($(this).position().top <= scrollDistance) {
-         $('#menu a li.selected').removeClass('selected');
-         $('#menu a li').eq(currentSectionIndex).addClass('selected');
-      }
-   });
+      const $sections = $('.section');
+      $sections.each(function (currentSectionIndex) {
+         if ($(this).position().top <= scrollDistance) {
+            $('#menu a li.selected').removeClass('selected');
+            $('#menu a li').eq(currentSectionIndex).addClass('selected');
+         }
+      });
+
+   }
 }).scroll();
