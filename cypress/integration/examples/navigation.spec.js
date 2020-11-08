2b5f0e081a792387404fd980250c40003a975715
Victor <vctr90w@gmail.com>
2020-11-08 09:59:44 -0500

    Added multiple resolutions to the test

--- cypress/integration/examples/navigation.spec.js
+++ cypress/integration/examples/navigation.spec.js
@@ -1,44 +1,22 @@
 /// <reference types="cypress" />
 
 context('Navigation', () => {
-  describe('Menu navigation', () => {
-    before(() => {
-      cy.visit('localhost:8000');
-    });
+  const sizes = ['iphone-x', [1920, 1080]];
 
-    describe('Default route', () => {
-      const menuElemetnToTest = 'hi';
-
-      it('Should select the correct menu element', () => {
-        cy.get(`a[href="#${menuElemetnToTest}"] li`)
-          .should('have.class', 'selected');
+  sizes.forEach(size => {
+    describe(`Menu navigation at ${size} resolution`, () => {
+      before(() => {
+        cy.visit('localhost:8000');
+        if (Cypress._.isArray(size)) {            
+          const [ width, height ] = size;
+          cy.viewport(width, height);
+        } else {            
+          cy.viewport(size);
+        }
       });
 
-      it('Should display the correct element on the screen', () => {
-        cy.get(`#${menuElemetnToTest}`)
-          .should('be.visible');
-      });
-    });
-
-    const menuElementsToTest = [
-      'hi',
-      'experience',
-      'technologies',
-      'portfolio',
-      'contact',
-      'where'
-    ];
-
-    menuElementsToTest.forEach((menuElemetnToTest) => {
-      describe(`Correct menu elements navigation ${menuElemetnToTest}`, () => {
-        before(() => {
-          cy.get(`a[href="#${menuElemetnToTest}"]`)
-            .click();
-        });
-
-        it('Should have the correct url', () => {
-          cy.url().should('include', `/#${menuElemetnToTest}`);
-        });
+      describe('Default route', () => {
+        const menuElemetnToTest = 'hi';
 
         it('Should select the correct menu element', () => {
           cy.get(`a[href="#${menuElemetnToTest}"] li`)
@@ -48,6 +26,38 @@ context('Navigation', () => {
         it('Should display the correct element on the screen', () => {
           cy.get(`#${menuElemetnToTest}`)
             .should('be.visible');
+        });
+      });
+
+      const menuElementsToTest = [
+        'hi',
+        'experience',
+        'technologies',
+        'portfolio',
+        'contact',
+        'where'
+      ];
+
+      menuElementsToTest.forEach((menuElemetnToTest) => {
+        describe(`Correct menu elements navigation ${menuElemetnToTest}`, () => {
+          before(() => {
+            cy.get(`a[href="#${menuElemetnToTest}"]`)
+              .click();
+          });
+
+          it('Should have the correct url', () => {
+            cy.url().should('include', `/#${menuElemetnToTest}`);
+          });
+
+          it('Should select the correct menu element', () => {
+            cy.get(`a[href="#${menuElemetnToTest}"] li`)
+              .should('have.class', 'selected');
+          });
+
+          it('Should display the correct element on the screen', () => {
+            cy.get(`#${menuElemetnToTest}`)
+              .should('be.visible');
+          });
         });
       });
     });
