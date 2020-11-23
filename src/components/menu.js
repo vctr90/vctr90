17ba39e9f82184ec203861fe1cf8c4e48f827c07
Victor <vctr90w@gmail.com>
2020-11-22 21:28:23 -0500

    Fix for chrome scroll issues

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,84 +2,52 @@ import React, { useEffect } from 'react';
 import $ from 'jquery';
 
 const InitializeMenuController = () => {
+  const fixChromeScrollIssue = () => {
+    setTimeout(function () {
+      const hash = window.location.hash;
+      window.location.hash = "";
+      window.location.hash = hash;
+    }, 300);
+  }
+  const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+  if (window.location.hash && isChrome) {
+    fixChromeScrollIssue();
+  }
   const $menuToggle = document.querySelector('#toggle-button');
   const $menu = document.querySelector('#menu');
-  const $menuElements = $menu.querySelectorAll('ul li');
-  //const manualScrollEvent = 'DOMMouseScroll mousewheel';
-  const manualScrollEvent = 'scroll';
 
   const triggerMenuToggle = () => $menu.classList.toggle('show-menu');
 
-
-  const navigateToSection = (event) => {
-    cleanMenuSelection();
-    event.currentTarget.classList.toggle('selected');
-    triggerMenuToggle();
-  };
-
-  const cleanMenuSelection = () => $menuElements.forEach(($menuElement) => $menuElement.classList.remove('selected'));
-
   $menuToggle.onclick = triggerMenuToggle;
-  //$menuElements.forEach(($menuElement) => $menuElement.onclick = navigateToSection);
 
-  const genericMenuElementSelector = `#menu a[href^="#"] li`;
   const selectedMenuElementSelector = `#menu a li.selected`;
-  const onScroll = (event) => {
-    //event.preventDefault();
-    const scrollPosition = $(document).scrollTop();
-    $(genericMenuElementSelector).each(function () {
-      const $menuElement = $(this);
-      const target = $menuElement.parent().attr('href');
-      const $menuElementTarget = $(target);
-      if($menuElementTarget.position().top <= scrollPosition && $menuElementTarget.position().top + $menuElementTarget.height() > scrollPosition) {
-        $(selectedMenuElementSelector).removeClass('selected');
-        $menuElement.addClass('selected');
-        $menuElementTarget.scrollTop(scrollPosition);
-      } else {
-        $menuElement.removeClass('selected');
+
+  // Implementation inspired from https://github.com/vctr90/cathypes.com/blame/master/src/components/shared/Header/NavMenu.js#L65
+
+  const onScroll = function () {
+    const scrollPosition = $(this).scrollTop() + 200;
+
+    const sections = $('.section');
+
+    sections.each(function () {
+      const $section = $(this);
+      const sectionTop = $section.offset().top;
+      const sectionBottom = sectionTop + $section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
+        if (scrollPosition <= sectionBottom) {
+          // Clean all selections
+          $(selectedMenuElementSelector).removeClass('selected');
+        }
+
+        // Select the matching section element
+        $(`#menu a[href="#${$section.attr('id')}"] li`).addClass('selected');
       }
     });
   };
 
   $(document).on('scroll', onScroll);
-
-  $(genericMenuElementSelector).on('click', function (event) {
-    event.preventDefault();
-    $(document).off('scroll');
-    
-    $(selectedMenuElementSelector).removeClass('selected');
-
-    $(this).addClass('selected');
-
-    const $target = $(this).parent();
-    const target = $target.attr('href');
-
-      window.location.hash = target;
-      $(document).on('scroll', onScroll);
-    /*$('html, body').stop().animate({
-      'scrollTop': $target.offset().top + 2
-    }, 200, 'swing', () => {
-      window.location.hash = target;
-      $(document).on('scroll', onScroll);
-    });*/
-  });
-  
-  /*$(window).on(manualScrollEvent, () => {
-    const scrollDistance = $(window).scrollTop();
-
-    const $sections = $('.section');
-    $sections.each(function($section) {
-      const bounding = $section.getBoundingClientRect();
-      if (bounding.top >= 0 && bounding.bottom <= window.innerHeight) {
-        console.log('scrollbar', scrollDistance);
-        console.log('this top', $($section).position().top);
-        console.log('--------------------------------------------------------');
-        $('#menu a li.selected').removeClass('selected');
-        $(`#menu a[href="#${$section.id}"] li`).addClass('selected');
-        return;
-      }
-    });
-  });*/
 };
 
 const Menu = () => {
